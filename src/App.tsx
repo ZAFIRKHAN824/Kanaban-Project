bd6a65f15cdc7a0964e0745781a0c590740d2778
ZAFIRKHAN824 <zafirkhan824@gmail.com>
2024-10-18 13:16:31 +0500

    done

--- src/App.tsx
+++ src/App.tsx
@@ -105,7 +105,7 @@ function App() {
             style={{
               width: "100%",
               height: 35,
-              fontSize: 30,
+              fontSize: 20,
               textAlign: "center",
             }}
             placeholder="Sorting"
